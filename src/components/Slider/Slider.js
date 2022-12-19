dd90b7d29b9e7c84c50ccc024722373c508a5c26
khalteck <khalidoyeneye@gmail.com>
2022-12-19 07:52:55 -0500

    slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,6 +5,7 @@ import { Autoplay, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css";
 import "swiper/css/pagination";
+import { Link } from "react-router-dom";
 
 function Slider() {
   const slider = sliderData.map((items) => {
@@ -19,10 +20,15 @@ function Slider() {
           <div className="space-y-4 lg:w-[65%]">
             <h3 className="text-lg font-semibold">{items.title}</h3>
             <p className="text-md font-light pb-8">{items.content}</p>
-            <button className="bg-[#1F66D0] font-medium text-white text-sm border-none px-4 py-1.5">
-              Sign up for free
-            </button>
-            <a className="text-[#1F66D0] block pb-8 lg:pb-24 cursor-pointer">
+            <Link to="/register">
+              <button className="bg-[#1F66D0] font-medium text-white text-sm border-none px-4 py-1.5">
+                Sign up for free
+              </button>
+            </Link>
+            <a
+              className="text-[#1F66D0] block pb-8 lg:pb-24 cursor-pointer"
+              href="/about"
+            >
               Learn more
             </a>
           </div>
@@ -36,7 +42,7 @@ function Slider() {
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
-        autoplay={{ delay: 7000, disableOnInteraction: false }}
+        autoplay={{ delay: 6000, disableOnInteraction: false }}
         pagination={{
           clickable: true,
         }}
