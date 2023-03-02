7cf846615273827aa313fa77a74cff3de0b37b79
dev-ayaa <ayaaakinleye@gmail.com>
2023-03-02 10:42:11 +0100

    update the CORS configuration

--- server/cmd/web/routes.go
+++ server/cmd/web/routes.go
@@ -10,26 +10,36 @@ import (
 
 func Routes(r *gin.Engine, o controller.Operator) {
 	r.MaxMultipartMemory = 10 << 20
+	// middleware for logging error and recovery from panic
 	router := r.Use(gin.Logger(), gin.Recovery())
 
-	router.Use(cors.Default())
+	// Setup CORS configuration
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"POST", "GET", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"}
+	router.Use(cors.New(config))
 
+	// Manage cookies data in session
 	cookieData := cookie.NewStore([]byte("travas"))
 	router.Use(sessions.Sessions("session", cookieData))
 
+	// HTTP method , URL for each endpoints
 	router.GET("/", o.Welcome())
 	router.GET("/register", o.Register())
 	router.POST("/register", o.ProcessRegister())
 	router.GET("/login", o.LoginPage())
 	router.POST("/login", o.ProcessLogin())
 	authRouter := r.Group("/auth")
+	// Restricted HTTP method requests and URL to an endpoints
 	authRouter.Use(Authorization())
 	{
+		// For operator to manage created tour packages
 		authRouter.POST("/verify/documents", o.VerifyDocument())
 		authRouter.POST("/add/packages", o.ProcessTourPackage())
 		authRouter.GET("/load/packages", o.LoadTourPackage())
 
-
+		// for Operator to manage tour guides
 		authRouter.GET("/guide/load", o.GetTourGuide())
 		authRouter.GET("/guide/select/assign", o.SelectTourGuide())
 		authRouter.POST("/guide/select/delete/:id", o.DeleteTourGuide())
